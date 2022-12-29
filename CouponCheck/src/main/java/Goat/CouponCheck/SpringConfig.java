8e03b7e75df3809fe4f195a7e8d0426b752e2fb7
Goathoon <ownest2@gmail.com>
2022-12-29 22:43:21 +0900

    doing jdbc repos

--- CouponCheck/src/main/java/Goat/CouponCheck/SpringConfig.java
+++ CouponCheck/src/main/java/Goat/CouponCheck/SpringConfig.java
@@ -1,13 +1,23 @@
 package Goat.CouponCheck;
 
-import Goat.CouponCheck.repository.MemoryRepository;
+import Goat.CouponCheck.repository.JdbcRepository;
 import Goat.CouponCheck.repository.Repository;
 import Goat.CouponCheck.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(repository());
@@ -15,6 +25,8 @@ public class SpringConfig {
 
     @Bean
     public Repository repository(){
-        return new MemoryRepository();
+
+//        return new MemoryRepository();
+        return new JdbcRepository(dataSource);
     }
 }
